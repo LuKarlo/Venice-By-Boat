7ac92d8a5943c10c76fc68e13926d61a7333ec94
Mirko Lurdo <lurdomirko07@gmail.com>
2026-04-29 14:38:00 +0200

    Enhance hero section with background image and badge; update NavBar CTA color to accent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,34 @@ export default function Home() {
   return (
     <div className="hero-container">
       <NavBar />
-      <div className="overlay-text">
-        <h1 className="main-title permanent-marker-regular">
-          VENEZIA <br /> <span className="evidence hand-drawn-underline">IN BARCA</span>
-        </h1>
-
-        <div className="brush-stroke-container">
-          <p>VIVI LA CITTÀ PIÙ BELLA DEL MONDO</p>
-          <p className="sub-text">DALL'ACQUA</p>
+      <div className="overlay">
+        <div className="text">
+          <h1 className="main-title permanent-marker-regular">
+            VENEZIA <br /> <span className="evidence">IN BARCA</span>
+          </h1>
         </div>
 
+        <div className="badge">
+          <div className="badge-circolare">
+            <div className="testo-curvo">
+              <svg viewBox="0 0 100 100">
+                <path
+                  id="circlePath"
+                  d="M 50, 50 m -37, 0 a 37,37 0 1,1 74,0 a 37,37 0 1,1 -74,0"
+                  fill="transparent"
+                />
+                <text>
+                  <textPath href="#circlePath">
+                   LOCALI PASSIONE • ESPERIENZE AUTENTICHE •
+                  </textPath>
+                </text>
+              </svg>
+            </div>
+            <div className="icona-barca">
+              <img src="/Venice-By-Boat/boat.svg" alt="Icona Barca" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
